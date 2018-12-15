3d25811683fc37c4e0647c5842048a8c2632ffa7
xiangzaizi <skyxiang.jiang@hotmail.com>
2018-12-15 10:55:25 +0800

    初始化设置+建库建表 +项目可运行调试

--- restframework/restframework/settings.py
+++ restframework/restframework/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+
+    'rest_framework',
+    'django_filters',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'restframework.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'restframework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_django_c09',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- restframework/users/models.py
+++ restframework/users/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    """部门模型类"""
+    name = models.CharField(max_length=20, verbose_name='部门名称')
+    create_date = models.DateField(verbose_name='成立时间')
+    is_delete = models.BooleanField(default=False, verbose_name='是否删除')
+
+    def __str__(self):
+        return self.name
+
+    class Meta(object):
+        db_table = 'department'
+
+
+class Employee(models.Model):
+    """员工模型类"""
+    choices_gender = (
+        (0, '男'),
+        (1, '女'),
+    )
+
+    name = models.CharField(verbose_name='姓名', max_length=20)
+    age = models.IntegerField(verbose_name='年龄')
+    gender = models.IntegerField(verbose_name='性别', default=0, choices=choices_gender)
+    salary = models.DecimalField(verbose_name='工资', max_digits=8, decimal_places=2)
+    comment = models.CharField(verbose_name='备注', max_length=300, null=True, blank=True)
+    hire_date = models.DateField(help_text='xx',verbose_name='入职时间', auto_now_add=True)
+    # 关联属性
+    department = models.ForeignKey('Department', verbose_name='所属部门')
+
+    def __str__(self):
+        return self.name
+
+    class Meta(object):
+        db_table = 'employee'
+
+
+class User(models.Model):
+    password = models.CharField(max_length=30)
